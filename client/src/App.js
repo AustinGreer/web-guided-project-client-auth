b259657904b1be86135a06859fbfb2ffed03b869
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-03-23 13:50:14 -0400

    lecture work-in-progress

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Login from './components/Login';
 import GasPrices from './components/GasPrices';
 
 import { axiosWithAuth } from './utils/axiosWithAuth';
-import axios from 'axios';
 
 function App() {
   const logout = () => {
@@ -13,18 +12,15 @@ function App() {
     //2. remove our local token
     //3. redirect to login page
 
-    axios.post('http://localhost:5000/api/logout', {
-      headers:{
-        authorization: localStorage.getItem('token')
-      }
-    })
-    .then(res => {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
-    })
-    .catch(err => {
-      console.log(err.response);
-    });
+    axiosWithAuth()
+      .post('http://localhost:5000/api/logout')
+      .then(res => {
+        localStorage.removeItem('token');
+        window.location.href = '/login';
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
   };
 
 
